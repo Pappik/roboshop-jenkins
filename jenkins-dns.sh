724de248b31d2589d811dc0eb177ddc0524b7946
pappik <pappik@gmail.com>
2023-12-27 13:09:41 -0500

    jenkins

--- jenkins-dns.sh
+++ jenkins-dns.sh
@@ -13,4 +13,4 @@ echo '
 }' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
 
 ZONE_ID="Z01783243D3S1K1FW0QID"
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file://tmp/jenkins.json | jq .
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
